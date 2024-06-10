7a394ca3f59d666e62f98dfb7da8853cbdf8a735
Yash Kumar Gupta <143840199+AuraReaper@users.noreply.github.com>
2024-06-10 22:30:39 +0530

    Delete C Programming directory

--- C Programming/Day 3/3.1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Name : Yash Kumar Gupta \n") ;
-    printf("Addres : KIIT KP-7AB \n") ;
-    printf("Detail : B.Tech CSE from KIIT University \n") ;
-}
